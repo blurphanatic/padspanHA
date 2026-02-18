563d8ec11b0eb103ea6e4959cc620ffb184f5495
Garry <53913193+gbroeckling@users.noreply.github.com>
2026-02-17 19:44:16 -0800

    Delete custom_components directory

--- custom_components/padspan_ha/exceptions.py
+++
@@ -1,8 +0,0 @@
-class PadSpanError(Exception):
-    """Base exception for PadSpan integration."""
-
-class PadSpanApiError(PadSpanError):
-    """PadSpan API returned an error."""
-
-class PadSpanApiConnectionError(PadSpanApiError):
-    """PadSpan API connection failed."""
