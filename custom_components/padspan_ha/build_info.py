809ab0520ba3aa1e5dfe81331765919cee3db2bb
Garry <53913193+gbroeckling@users.noreply.github.com>
2026-02-19 16:30:26 -0800

    Add files via upload

--- custom_components/padspan_ha/build_info.py
+++ custom_components/padspan_ha/build_info.py
@@ -1,7 +1,7 @@
 """Generated at build time. Used to prove what version is actually installed."""
 
-BUILD_VERSION = "0.4.16"
-BUILD_ID = "20260219T233756Z"
+BUILD_VERSION = "0.4.2"
+BUILD_ID = "20260220T002500Z"
 
 # Backwards/for convenience
 VERSION = BUILD_VERSION
