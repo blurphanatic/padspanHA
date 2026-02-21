7792c3920278093f9810648ba8cb8d0000c03d9f
Garry <53913193+gbroeckling@users.noreply.github.com>
2026-02-21 13:08:26 -0800

    chore: bump version to v0.4.34

--- custom_components/padspan_ha/build_info.py
+++ custom_components/padspan_ha/build_info.py
@@ -1,7 +1,7 @@
 """Generated at build time. Used to prove what version is actually installed."""
 
-BUILD_VERSION = "0.4.33"
-BUILD_ID = "20260221T194321Z"
+BUILD_VERSION = "0.4.34"
+BUILD_ID = "20260221T210826Z"
 
 # Backwards/for convenience
 VERSION = BUILD_VERSION
