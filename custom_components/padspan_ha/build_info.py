c792815b8761e673ffc4a388f0d9c5f035526084
Garry <53913193+gbroeckling@users.noreply.github.com>
2026-02-21 11:43:21 -0800

    chore: bump version to v0.4.33

--- custom_components/padspan_ha/build_info.py
+++ custom_components/padspan_ha/build_info.py
@@ -1,7 +1,7 @@
 """Generated at build time. Used to prove what version is actually installed."""
 
-BUILD_VERSION = "0.4.32"
-BUILD_ID = "20260221T074807Z"
+BUILD_VERSION = "0.4.33"
+BUILD_ID = "20260221T194321Z"
 
 # Backwards/for convenience
 VERSION = BUILD_VERSION
--- custom_components/padspan_ha/www/padspan-ha/panel.js
+++ custom_components/padspan_ha/www/padspan-ha/panel.js
@@ -34,9 +34,9 @@ import * as Diagnostics from "./views/diagnostics.js?b=20260221T120000Z";
 import * as QA from "./views/qa.js?b=20260221T120000Z";
 import * as Sandbox from "./views/sandbox.js?b=20260221T120000Z";
 
-const APP_VERSION = "0.4.32";
+const APP_VERSION = "0.4.33";
 // Build stamp used for cache-busting and Diagnostics.
-const BUILD_ID = "20260221T120000Z";
+const BUILD_ID = "20260221T194321Z";
 
 const VIEWS = {
   follow: Follow,
