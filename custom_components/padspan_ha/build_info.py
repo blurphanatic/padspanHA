0633d1e94e91645ec381649f6b2e1aa6f1f32725
Garry <53913193+gbroeckling@users.noreply.github.com>
2026-03-20 15:31:29 -0700

    chore: bump version to v0.15.62

--- custom_components/padspan_ha/build_info.py
+++ custom_components/padspan_ha/build_info.py
@@ -4,8 +4,8 @@
 # See LICENSE file or https://www.gnu.org/licenses/gpl-3.0.html
 """Generated at build time. Used to prove what version is actually installed."""
 
-BUILD_VERSION = "0.15.61"
-BUILD_ID = "20260320T222158Z"
+BUILD_VERSION = "0.15.62"
+BUILD_ID = "20260320T223128Z"
 CHANNEL = "beta"
 
 # Backwards/for convenience
